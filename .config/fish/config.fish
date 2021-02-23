7a3a83ab1142fee791ebb712dab1c06a20b5b541
oza6ut0ne <33759728+oza6ut0ne@users.noreply.github.com>
2021-02-23 13:37:28 +0900

    add color config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,6 +8,7 @@ if test -f /etc/profile.d/apps-bin-path.sh
     bass source /etc/profile.d/apps-bin-path.sh
 end
 
+# appearance
 set -x TERM screen-256color
 set -x fish_greeting
 
@@ -18,9 +19,29 @@ set -x fish_color_quote yellow
 set -x fish_color_operator bryellow
 set -x fish_color_redirection 00afff
 
+alias ls='ls --color=auto'
+alias dir='dir --color=auto'
+alias vdir='vdir --color=auto'
+alias diff='diff --color=auto'
+alias ip='ip --color'
+
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+
+set -x LESS_TERMCAP_mb (printf '\e[1;31m')     # begin blink
+set -x LESS_TERMCAP_md (printf '\e[1;36m')     # begin bold
+set -x LESS_TERMCAP_me (printf '\e[0m')        # reset bold/blink
+set -x LESS_TERMCAP_so (printf '\e[01;33m')    # begin reverse video
+set -x LESS_TERMCAP_se (printf '\e[0m')        # reset reverse video
+set -x LESS_TERMCAP_us (printf '\e[1;4;32m')   # begin underline
+set -x LESS_TERMCAP_ue (printf '\e[0m')        # reset underline
+
+# editor
 set -x EDITOR nvim
 set -x SUDO_EDITOR 'nvim -R'
 
+# aliases
 alias ll='ls -alF'
 alias cp='cp -i'
 alias mv='mv -i'
@@ -28,15 +49,15 @@ alias rm='rm -i'
 alias crontab='crontab -i'
 alias vi nvim
 
+if which adb-peco >/dev/null 2>&1
+    balias adb adb-peco
+end
+
 set -x PATH "$HOME/go/bin" $PATH
 
 # anyenv
 set -x PATH  "$HOME/.anyenv/bin" $PATH
 anyenv init - fish | grep -v '..env rehash' | source
-
-if which adb-peco >/dev/null 2>&1
-    balias adb adb-peco
-end
 
 # direnv
 if which direnv >/dev/null 2>&1
