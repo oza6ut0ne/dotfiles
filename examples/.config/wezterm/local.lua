d258341564b0b760ac3b8dff0b31e7a6a0469e68
oza6ut0ne <33759728+oza6ut0ne@users.noreply.github.com>
2025-08-11 01:00:48 +0900

    Update examples

---
+++ examples/.config/wezterm/local.lua
@@ -0,0 +1,13 @@
+local wezterm = require 'wezterm'
+local helpers = require 'helpers'
+
+local module = {}
+
+function module.apply(config)
+  -- config.front_end = 'WebGpu'
+  -- config.front_end = 'OpenGL'
+  -- config.enable_scroll_bar = true
+  -- config.line_height = 1.0
+end
+
+return module
